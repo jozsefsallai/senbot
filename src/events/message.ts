5212d0adb5f083f6f8b2f61079a63881d61f8b9a
József Sallai <jozsef@sallai.me>
2023-04-01 17:13:29 +0300

    emote thing

--- src/events/message.ts
+++ src/events/message.ts
@@ -3,6 +3,26 @@ import Client from '../core/client';
 import analyzeMessage from '../utils/analyzeMessage';
 
 const handler = async (client: Client, message: Message | PartialMessage) => {
+  try {
+    const emotes = await message.guild?.emojis.fetch();
+    if (!emotes) {
+      return;
+    }
+
+    const probability = message.content?.includes('uwu')
+      ? 1
+      : 1 - Math.random();
+    if (probability >= 0.95) {
+      const count = probability >= 0.99 ? 3 : 1;
+      const reactions = emotes.random(count);
+
+      const promises = [reactions.map((e) => message.react(e))];
+      await Promise.all(promises);
+    }
+  } catch (err) {
+    // rip
+  }
+
   await analyzeMessage(client, message);
 };
 
